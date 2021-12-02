af8b58e6a66fd8ea654c1748e9eaa614b66707c0
lipan6461188 <lee@lipan.local>
2021-12-02 10:17:11 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,105 @@
+# Run AlphaFold-Multimer step by step
+
+AlphaFold-Multimer running process is splited into four parts:
+
+1. Search homologous sequences and templates
+2. Run models 1-5 to produce the unrelaxed models
+3. Relax models
+4. Sort models by confidence score
+
+### Usage
+
+1. Install AlphaFold2 (https://github.com/deepmind/alphafold)
+
+2. Install Python packages
+
+   ```bash
+   pip install -U scikit-learn
+   pip install -U matplotlib
+   pip install -U seaborn
+   ```
+
+3. Download `AlphaFold-StepByStep`
+
+   ```bash
+   git clone https://github.com/lipan6461188/AlphaFold-StepByStep.git
+   cd AlphaFold-StepByStep
+   chmod +x *.py
+   export PATH=$(pwd):$PATH
+   ```
+
+4. Set `config.ini`
+
+   ```ini
+   [ALPHAFOLD2]
+   alphafold_path = /path/to/alphafold-2.1.0
+   
+   [EXCUTABLE]
+   jackhmmer_binary_path       = 
+   hhblits_binary_path         = 
+   hhsearch_binary_path        = 
+   hmmsearch_binary_path       = 
+   hmmbuild_binary_path        = 
+   kalign_binary_path          = 
+   
+   [DATABASE]
+   uniref90_database_path      = /AF2_db/uniref90/uniref90.fasta
+   mgnify_database_path        = /AF2_db/mgnify/mgy_clusters.fa
+   template_mmcif_dir          = /AF2_db/pdb_mmcif/mmcif_files
+   obsolete_pdbs_path          = /AF2_db/pdb_mmcif/obsolete.dat
+   uniprot_database_path       = /AF2_db/uniprot/uniprot.fasta
+   pdb_seqres_database_path    = /AF2_db/pdb_mmcif/pdb_seqres_old.txt
+   uniclust30_database_path    = /AF2_db/uniclust30/UniRef30_2020_02
+   bfd_database_path           = /AF2_db/bfd/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt
+   ```
+
+5. Run `AlphaFold-StepByStep`
+
+   Run the complete process：
+
+   ```bash
+   run_af_multimer.py examples/4HHB_complex.fasta examples/output
+   # Add --is_prokaryote is the protein sequences are from prokaryote
+   # Add --skip_refine to skip the relax process
+   ```
+
+   Optional, you can run AlphaFold-Multimer step by step：
+
+   ```bash
+   # Step 1: Search homologous sequences and templates (Use CPU)
+   run_af_multimer_step1.py examples/4HHB_complex.fasta examples/output/
+   
+   # Step 2: Run models 1-5 to produce the unrelaxed models (Use GPU)
+   export CUDA_VISIBLE_DEVICES="1" # which GPU card to use
+   run_af_multimer_step2.py examples/output/features.pkl examples/output/
+   
+   # Step 3: Relax models (Use CPU)
+   for idx in $(seq 1 5);
+   do
+   	run_af_multimer_step3.py \
+     		examples/output/features.pkl \
+         examples/output/result_model_${idx}_multimer.pkl \
+         examples/output/relaxed_model_${idx}_multimer.pdb
+   done
+   
+   # Step 4: Sort and visualize (Use CPU)
+   cd examples/output
+   run_af_multimer_step4.py result_model_1_multimer.pkl,result_model_2_multimer.pkl,result_model_3_multimer.pkl,result_model_4_multimer.pkl,result_model_5_multimer.pkl relaxed_model_1_multimer.pdb,relaxed_model_2_multimer.pdb,relaxed_model_3_multimer.pdb,relaxed_model_4_multimer.pdb,relaxed_model_5_multimer.pdb ./
+   ```
+
+### Outputs
+
+#### predicted_aligned_error
+
+![1.png](examples/output/model_1_predicted_aligned_error.png)
+
+#### pLDDT
+
+![2.png](examples/output/model_2_plddt.png)
+
+#### Protein structure
+
+![3.png](imgs/3.png)
+
+Visualize the pLDDT with command `rangecolor bfactor, 50 #f08253 70 #fada4d 90 #7ec9ef 100 #1b57ce` in [UCSF Chimera](https://www.cgl.ucsf.edu/chimera/).
+
---
+++ run_af_multimer.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+###############################################
+#
+#
+#   Run AlphaFold-Multimer step by step
+#   (https://github.com/deepmind/alphafold)
+#   Author: Pan Li (lipan@shuimubio.com)
+#                    @ Shuimu BioScience
+#        https://www.shuimubio.com/
+#
+#
+################################################
+
+#
+#
+#  AlphaFold-Multimer Step 1-4
+#  Usage: run_af_multimer.py [--is_prokaryote] [--skip_refine] /path/to/input.fasta /path/to/output
+#   Step 1: Search homologous sequences and templates
+#   Step 2: Run models 1-5 to produce the unrelaxed models
+#   Step 3: Relax models
+#   Step 4: Sort models
+#
+#
+
+running_str = """
+Run AlphaFold-Multimer step by step
+# Step 1: Search homologous sequences and templates
+# Step 2: Run models 1-5 to produce the unrelaxed models
+# Step 3: Relax models. Very slow for large protein complex
+# Step 4: Sort models
+"""
+
+import json
+import os
+import pathlib
+import pickle
+import random
+import shutil
+import sys
+import time
+import gzip
+from typing import Dict, Union, Optional
+import numpy as np
+import argparse
+
+cur_path = pathlib.Path(__file__).parent.resolve()
+parser = argparse.ArgumentParser(description='AlphaFold-Multimer pipeline')
+
+parser.add_argument('input_file', metavar='input_file', type=str, help='The fasta file to process, must contain multiple sequences.')
+parser.add_argument('output_dir', metavar='output_dir', type=str, help='Path to a directory that will store the results.')
+parser.add_argument('--is_prokaryote', action='store_true', help='The input protein sequences are from prokaryote species.')
+parser.add_argument('--skip_refine', action='store_true', help='Skip the refine step (step 3)')
+
+args = parser.parse_args()
+
+print(running_str, flush=True)
+
+step1_file = os.path.join(cur_path, 'run_af_multimer_step1.py')
+step2_file = os.path.join(cur_path, 'run_af_multimer_step2.py')
+step3_file = os.path.join(cur_path, 'run_af_multimer_step3.py')
+step4_file = os.path.join(cur_path, 'run_af_multimer_step4.py')
+
+########################
+### Step 1
+########################
+
+cmd = f"python {step1_file} {args.input_file} {args.output_dir}"
+if args.is_prokaryote:
+    cmd += ' --is_prokaryote'
+
+print("Run Step 1: Search homologous sequences and templates")
+if os.system(cmd) != 0:
+    print("Step 1 Failed")
+    exit(-1)
+
+########################
+### Step 2
+########################
+
+cmd = f"python {step2_file} {args.output_dir}/features.pkl {args.output_dir}"
+
+print("Run Step 2: Run models 1-5 to produce the unrelaxed models")
+if os.system(cmd) != 0:
+    print("Step 2 Failed")
+    exit(-1)
+
+########################
+### Step 3
+########################
+
+print("Run Step 3: Relax models. Very slow for large protein complex")
+if args.skip_refine:
+    print("Step 3 skipped")
+else:
+    for i in range(1, 6):
+        cmd = f"python {step3_file} {args.output_dir}/features.pkl {args.output_dir}/result_model_{i}_multimer.pkl {args.output_dir}/relaxed_model_{i}_multimer.pdb"
+        if os.system(cmd) != 0:
+            print("Step 3 Failed")
+            exit(-1)
+
+########################
+### Step 4
+########################
+
+model_pkls = ",".join([ f'{args.output_dir}/result_model_{i}_multimer.pkl' for i in range(1,6) ])
+
+if args.skip_refine:
+    model_pdbs = ",".join([ f'{args.output_dir}/unrelaxed_model_{i}_multimer.pdb' for i in range(1,6) ])
+else:
+    model_pdbs = ",".join([ f'{args.output_dir}/relaxed_model_{i}_multimer.pdb' for i in range(1,6) ])
+
+cmd = f"python {step4_file} {model_pkls} {model_pdbs} {args.output_dir}"
+
+print("Run Step 4: Sort models")
+if os.system(cmd) != 0:
+    print("Step 4 Failed")
+    exit(-1)
+
+
